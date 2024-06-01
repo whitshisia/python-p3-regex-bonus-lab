d8deac62c913257e9a1e3f88e83bfe2eaf0236fd
whitney-shisia <shisiawhitney215@gmail.com>
2024-06-01 19:24:38 +0300

    py

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,10 @@
 import re
+patterns = [
+    "It's such a lovely day today.",
+    "Some weather we're having today, huh?",
+    "Maybe today's just not my day."
+]
+my_pattern = '|'.join(re.escape(pattern) for pattern in patterns)
 
-my_pattern = r""
 my_regex = re.compile(my_pattern)
 
